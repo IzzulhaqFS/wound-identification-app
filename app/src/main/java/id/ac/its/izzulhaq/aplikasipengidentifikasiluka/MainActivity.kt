c293ae831d1fc62f4ab505d6f64b801ece16f95f
IzzulhaqFS <izzulhaq219@gmail.com>
2023-09-28 23:02:28 +0700

    feat: add camera access

--- app/src/main/java/id/ac/its/izzulhaq/aplikasipengidentifikasiluka/MainActivity.kt
+++ app/src/main/java/id/ac/its/izzulhaq/aplikasipengidentifikasiluka/MainActivity.kt
@@ -2,10 +2,29 @@ package id.ac.its.izzulhaq.aplikasipengidentifikasiluka
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnCamera: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportActionBar?.title = TITLE
+
+        btnCamera = findViewById(R.id.btn_camera)
+
+        btnCamera.setOnClickListener {
+            startCamera()
+        }
+    }
+
+    private fun startCamera() {
+
+    }
+
+    companion object {
+        private const val TITLE = "Home"
     }
 }
