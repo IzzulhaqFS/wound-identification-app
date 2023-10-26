d53fc9b032b00682786f04acde9e785e90bb77d3
IzzulhaqFS <izzulhaq219@gmail.com>
2023-10-26 17:28:35 +0700

    feat: add HistoryActivity

---
+++ app/src/main/java/id/ac/its/izzulhaq/aplikasipengidentifikasiluka/activities/HistoryActivity.kt
@@ -0,0 +1,12 @@
+package id.ac.its.izzulhaq.aplikasipengidentifikasiluka.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.ac.its.izzulhaq.aplikasipengidentifikasiluka.R
+
+class HistoryActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history)
+    }
+}
